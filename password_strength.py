00401fa51473bd34f350d7acccaedb6389a72d52
Nurbek <nurbek@inbox.ru>
2017-09-28 16:22:22 +0400

    Fixed algorythm.Simplification

--- password_strength.py
+++ password_strength.py
@@ -14,25 +14,20 @@ def get_password_strength(password):
     for symbol in password:
         if low_case_letters == False and 97 <= ord(symbol) <= 122:
             low_case_letters = True
+            score += 2
         if up_case_letters == False and 65 <= ord(symbol) <= 90:
             up_case_letters = True
+            score += 3
         elif numerical_digits == False and 48 <= ord(symbol) <= 57:
             numerical_digits = True
+            score += 3
         elif special_characters == False and 33 <= ord(symbol) <= 47 or 58 <= ord(symbol) <= 64 or \
             91 <= ord(symbol) <= 96 or 123 <= ord(symbol) <= 126:
             special_characters = True
+            score += 3
     if low_case_letters and up_case_letters and numerical_digits and special_characters:
         score = 10
         return score
-    if low_case_letters:
-        score += 2
-    if up_case_letters:
-        score += 3
-    if numerical_digits:
-        score += 3
-    if special_characters:
-        score += 3
-    return score
 
 
 if __name__ == '__main__':
