5c55c29e06b8e681367e8e9eb332491cf77d9349
Nurbek <nurbek@inbox.ru>
2017-09-28 12:17:50 +0400

    'Release'

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+The script shows level of password strength from 1(very weak) to 10(very strong)
+
+# Quickstart
+Launch on Linux( Python 3.5 ) or Windows as simple
+```#!bash
+$ python password_strength.py <password>
+# For example, python password_strength.py 12345
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,43 @@
+import sys
 
 
 def get_password_strength(password):
-    pass
+    blacklist = ['qwerty','12345','123456','Password1','qwaszx','1q2w3e4r']
+    low_case_letters, up_case_letters, numerical_digits, special_characters = False, False, False, False
+    score = 0
+    if password in blacklist:
+        score += 1
+        return score
+    if password.isdigit():
+        score = +2
+        return score
+    for symbol in password:
+        if low_case_letters == False and 97 <= ord(symbol) <= 122:
+            low_case_letters = True
+        if up_case_letters == False and 65 <= ord(symbol) <= 90:
+            up_case_letters = True
+        elif numerical_digits == False and 48 <= ord(symbol) <= 57:
+            numerical_digits = True
+        elif special_characters == False and 33 <= ord(symbol) <= 47 or 58 <= ord(symbol) <= 64 or \
+            91 <= ord(symbol) <= 96 or 123 <= ord(symbol) <= 126:
+            special_characters = True
+    if low_case_letters and up_case_letters and numerical_digits and special_characters:
+        score = 10
+        return score
+    if low_case_letters:
+        score += 2
+    if up_case_letters:
+        score += 3
+    if numerical_digits:
+        score += 3
+    if special_characters:
+        score += 3
+    return score
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        password = sys.argv[1]
+        print('Password strength: ' + str(get_password_strength(password)))
+    else:
+        print('Error! Enter password!')
