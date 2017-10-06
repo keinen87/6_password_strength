a742763064d36fbb2e4a1b1a9d73695e43af0c29
Nurbek <nurbek@inbox.ru>
2017-10-06 10:05:29 +0400

    Simplification algorytm

--- password_strength.py
+++ password_strength.py
@@ -3,31 +3,20 @@ import sys
 
 def get_password_strength(password):
     blacklist = ['qwerty','12345','123456','Password1','qwaszx','1q2w3e4r']
-    low_case_letters, up_case_letters, numerical_digits, special_characters = False, False, False, False
     score = 0
+    low_cs_let, up_cs_let, num_dig, spec_char = False, False, False, False
     if password in blacklist:
         score += 1
         return score
     if password.isdigit():
-        score += 2
+        score += 2.5
         return score
-    for symbol in password:
-        if low_case_letters == False and 97 <= ord(symbol) <= 122:
-            low_case_letters = True
-            score += 2
-        if up_case_letters == False and 65 <= ord(symbol) <= 90:
-            up_case_letters = True
-            score += 3
-        elif numerical_digits == False and 48 <= ord(symbol) <= 57:
-            numerical_digits = True
-            score += 3
-        elif special_characters == False and 33 <= ord(symbol) <= 47 or 58 <= ord(symbol) <= 64 or \
-            91 <= ord(symbol) <= 96 or 123 <= ord(symbol) <= 126:
-            special_characters = True
-            score += 3
-    if low_case_letters and up_case_letters and numerical_digits and special_characters:
-        score = 10
-        return score
+    content = { 'low_cs_let': True for symbol in password if 97 <= ord(symbol) <= 122 and not low_cs_let }
+    content.update({'up_cs_let': True for symbol in password if 65 <= ord(symbol) <= 90 and not up_cs_let })
+    content.update({'num_dig': True for symbol in password if 48 <= ord(symbol) <= 57 and not num_dig })
+    content.update({'spec_char': True for symbol in password if (33 <= ord(symbol) <= 47 \
+     or 58 <= ord(symbol) <= 64 or 91 <= ord(symbol) <= 96 or 123 <= ord(symbol) <= 126) and not spec_char })
+    score = 2.5 * len(content.values())
     return score
 
 
