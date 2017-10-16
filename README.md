c63ff457a6c9dd2461eb1e9e960e92abec0a18cd
Nurbek <nurbek@inbox.ru>
2017-10-16 14:05:46 +0400

    Move logic of load_data to if-main func and rename all checking functions

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 The script shows level of password strength from 1(very weak) to 10(very strong)
 
 # Quickstart
+
 Before checking your password you may to download blacklist.txt file from this repository to folder
-with script (or to another place - on Windows systems replace '\' on '/' at input the path) or create own file(file name at your discretion)
+with script or create own file(file name at your discretion)
 For example, we create blacklist.txt with contents:
 >12345
 >qwerty
@@ -13,14 +14,9 @@ For example, we create blacklist.txt with contents:
 Launch on Linux( Python 3.5 ) or Windows as simple
 ```bash
 $ python password_strength.py blacklist.txt
-# You need to enter password(password is shielded)
-Password:
-# For example, we checking easy password 12345, it in our blacklist
-# python password_strength.py blacklist.txt
 # You will see
 Password: *****
-# You will see
-Password strength: 0
+Password strength: 5
 ```
 
 # Project Goals
--- password_strength.py
+++ password_strength.py
@@ -1,6 +1,5 @@
 import argparse
 import getpass
-import os
 import string
 
 
@@ -14,28 +13,27 @@ def is_password_in_blacklist(password, blacklist):
     return password in blacklist
 
 
-def is_password_on_lowercase(password):
+def is_password_in_lowercase(password):
     return any(symbol in string.ascii_lowercase for symbol in password)
 
 
-def is_password_on_uppercase(password):
+def is_password_in_uppercase(password):
     return any(symbol in string.ascii_uppercase for symbol in password)
 
 
-def is_password_on_digit_symbols(password):
+def is_password_in_digit_symbols(password):
     return any(symbol in string.digits for symbol in password)
 
 
-def is_password_on_special_symbols(password):
+def is_password_in_special_symbols(password):
     return any(symbol in string.punctuation for symbol in password)
 
 
-def get_password_strength(password):
+def get_password_strength(password, blacklist):
     score = 0
     min_length_bound = 5
     max_lenght_bound = 8
-    if is_password_in_blacklist(password, load_blacklist_file(filepath)) or \
-            len(password) < min_length_bound:
+    if blacklist or len(password) < min_length_bound:
         return score
     score = sum([is_password_on_lowercase(password),
                  is_password_on_uppercase(password),
@@ -53,4 +51,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
     filepath = args.filepath
     password = getpass.getpass()
-    print('Password strength: {0}'.format(get_password_strength(password)))
+    print('Password strength: {0}'.format(get_password_strength(
+        password,
+        is_password_in_blacklist(password, load_blacklist_file(filepath)))))
